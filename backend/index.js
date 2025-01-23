8b5f381720a286adfbc5f9f06bd76fdccb1b4ae2
dhaxanxxt <mohn08052006@gmail.com>
2025-01-23 10:49:09 +0530

    ping Route Added Successfully

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.get('/ping', (req, res) =>{
+    try {
+        res.status(200).send("Ping Working Fine")
+    } catch (error) {
+        res.status(400).send("Ping Not Working Fine")
+    }
+})
 
 app.listen(3000, (req, res) =>{
     try {
