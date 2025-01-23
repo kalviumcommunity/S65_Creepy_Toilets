e5866e42f9cc49b64ee9dc717d490147352742ea
dhaxanxxt <mohn08052006@gmail.com>
2025-01-23 10:27:48 +0530

    Added Server Error Handler

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ const app = express()
 
 app.listen(3000, (req, res) =>{
     try {
-        
         console.log("Server is Listening at Port 3000")
     } catch (error) {
         console.log(error.message)
