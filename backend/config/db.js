2df396c9e713e9a7bb56863b86a057d18f4f9b86
dhaxanxxt <mohn08052006@gmail.com>
2025-02-11 10:29:07 +0530

    Mongodb Atlas and Entities Added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose')
 
 const connetDB = async () =>{
     try {
-        await mongoose.connect()
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Database Connected Successfully !!')
     } catch (error) {
-        
+        console.error("Server Error", error)
     }
 }
+
+module.exports = connetDB
